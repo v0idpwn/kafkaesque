c3f234d9bbde985f6d4002159e268062308fb557
v0idpwn <v0idpwn@gmail.com>
2021-03-07 18:52:30 -0300

    Prepare for publishing

--- mix.exs
+++ mix.exs
@@ -1,26 +1,30 @@
 defmodule Kafkaesque.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+
   def project do
     [
       app: :kafkaesque,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       elixirc_paths: elixirc_paths(Mix.env()),
-      deps: deps()
+      deps: deps(),
+      description: description(),
+      package: package(),
+      name: "Kafkaesque",
+      source_url: "https://github.com/v0idpwn/kafkaesque"
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:brod, "~> 3.15"},
@@ -28,7 +32,21 @@ defmodule Kafkaesque.MixProject do
       {:ecto_sql, ">= 3.4.3"},
       {:jason, "~> 1.1"},
       {:postgrex, "~> 0.14"},
-      {:telemetry, "~> 0.4"}
+      {:telemetry, "~> 0.4"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    "Transactional outbox for kafka"
+  end
+
+  defp package do
+    [
+      name: "kafkaesque",
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/v0idpwn/kafkaesque"}
     ]
   end
 
