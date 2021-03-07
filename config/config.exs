4e74832d7d34246b5c400bc2bb1a3422b47d6347
v0idpwn <v0idpwn@gmail.com>
2021-03-07 19:39:35 -0300

    Fix testing

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :kafkaesque, Kafkaesque.Test.Repo,
   priv: "test/support/",
-  url: "postgres://postgres@localhost:5432/oban_test",
+  url: "postgres://postgres@localhost:5432/kafkaesque_test",
   password: "postgres",
   pool_size: 10
 
--- test/kafkaesque_test.exs
+++ test/kafkaesque_test.exs
@@ -2,7 +2,4 @@ defmodule KafkaesqueTest do
   use ExUnit.Case
   doctest Kafkaesque
 
-  test "greets the world" do
-    assert Kafkaesque.hello() == :world
-  end
 end
