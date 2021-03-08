cf9ad6be52e31e4a23e8c5cf1f68e8142f1e7594
v0idpwn <v0idpwn@gmail.com>
2021-03-07 22:02:27 -0300

    Public API test

--- test/kafkaesque_test.exs
+++ test/kafkaesque_test.exs
@@ -1,4 +1,19 @@
 defmodule KafkaesqueTest do
-  use ExUnit.Case
-  doctest Kafkaesque
+  use Kafkaesque.Case
+
+  alias Kafkaesque.Test.Repo
+
+  describe "publish/3" do
+    @tag :integration
+    test "inserts valid messages" do
+      {:ok, %Kafkaesque.Message{}} = Kafkaesque.publish(Repo, "test_topic", %{})
+    end
+
+    test "errors for invalid messages" do
+      invalid_topic = 1
+      invalid_body = {1, 2}
+
+      assert {:error, %Ecto.Changeset{}} = Kafkaesque.publish(Repo, invalid_topic, invalid_body)
+    end
+  end
 end
